add4f9184a1b68caa40a8ab815a39eb6e1cf926a
Xamidullo Xudoyberdiyev <khkhamidullo@gmail.com>
2023-09-10 16:03:34 +0500

    uodate

--- src/main.ts
+++ src/main.ts
@@ -13,7 +13,10 @@ const MONGO_URI =
 function dev() {
     try {
         mongoose
-            .connect(MONGO_URI)
+            .connect(MONGO_URI, {
+                useNewUrlParser: true,
+                useUnifiedTopology: true,
+            })
             .then(() => console.log("Mongodb Connect"))
             .catch((error) => console.log(error));
 
@@ -21,6 +24,7 @@ function dev() {
             console.log("Server running on port 3000");
         });
     } catch (error) {
+        dev();
         console.log(error);
     }
 }
